87a7071fa320999bbff62a9ce7bb35c96e9195ae
aptikasijunjung-crypto <aptikasijunjung@gmail.com>
2026-04-16 11:24:21 +0700

    second commit

--- resources/views/frontend/template.blade.php
+++ resources/views/frontend/template.blade.php
@@ -251,87 +251,17 @@ License: You must have a valid license purchased only from themeforest(the above
                             </div>
                             <!--end::Tablet & Mobile Search-->
                             <!--begin::Create-->
-                            <div class="dropdown">
-                                <!--begin::Toggle-->
-                                <div class="topbar-item mr-4" data-toggle="dropdown" data-offset="10px,0px">
-                                    <div class="btn font-weight-bolder btn-sm btn-light-success px-5">Create</div>
-                                </div>
-                                <!--end::Toggle-->
-                                <!--begin::Dropdown-->
-                                <div
-                                    class="dropdown-menu p-0 m-0 dropdown-menu-right dropdown-menu-anim-up dropdown-menu-md">
-                                    <!--begin::Navigation-->
-                                    <ul class="navi navi-hover py-5">
-                                        <li class="navi-item">
-                                            <a href="#" class="navi-link">
-                                                <span class="navi-icon">
-                                                    <i class="flaticon2-drop"></i>
-                                                </span>
-                                                <span class="navi-text">New Group</span>
-                                            </a>
-                                        </li>
-                                        <li class="navi-item">
-                                            <a href="#" class="navi-link">
-                                                <span class="navi-icon">
-                                                    <i class="flaticon2-list-3"></i>
-                                                </span>
-                                                <span class="navi-text">Contacts</span>
-                                            </a>
-                                        </li>
-                                        <li class="navi-item">
-                                            <a href="#" class="navi-link">
-                                                <span class="navi-icon">
-                                                    <i class="flaticon2-rocket-1"></i>
-                                                </span>
-                                                <span class="navi-text">Groups</span>
-                                                <span class="navi-link-badge">
-                                                    <span
-                                                        class="label label-light-primary label-inline font-weight-bold">new</span>
-                                                </span>
-                                            </a>
-                                        </li>
-                                        <li class="navi-item">
-                                            <a href="#" class="navi-link">
-                                                <span class="navi-icon">
-                                                    <i class="flaticon2-bell-2"></i>
-                                                </span>
-                                                <span class="navi-text">Calls</span>
-                                            </a>
-                                        </li>
-                                        <li class="navi-item">
-                                            <a href="#" class="navi-link">
-                                                <span class="navi-icon">
-                                                    <i class="flaticon2-gear"></i>
-                                                </span>
-                                                <span class="navi-text">Settings</span>
-                                            </a>
-                                        </li>
-                                        <li class="navi-separator my-3"></li>
-                                        <li class="navi-item">
-                                            <a href="#" class="navi-link">
-                                                <span class="navi-icon">
-                                                    <i class="flaticon2-magnifier-tool"></i>
-                                                </span>
-                                                <span class="navi-text">Help</span>
-                                            </a>
-                                        </li>
-                                        <li class="navi-item">
-                                            <a href="#" class="navi-link">
-                                                <span class="navi-icon">
-                                                    <i class="flaticon2-bell-2"></i>
-                                                </span>
-                                                <span class="navi-text">Privacy</span>
-                                                <span class="navi-link-badge">
-                                                    <span
-                                                        class="label label-light-danger label-rounded font-weight-bold">5</span>
-                                                </span>
-                                            </a>
-                                        </li>
-                                    </ul>
-                                    <!--end::Navigation-->
-                                </div>
-                                <!--end::Dropdown-->
+
+
+                            <!--begin::Toggle-->
+                            <div class="topbar-item mr-4">
+                                <a href="ddd" class="btn font-weight-bolder btn-sm btn-light-success px-5">sd</a>
                             </div>
+                            <!--end::Toggle-->
+                            <!--begin::Dropdown-->
+
+                            <!--end::Dropdown-->
+
                             <!--end::Create-->
                             <!--begin::Quick Panel-->
 
@@ -843,7 +773,7 @@ License: You must have a valid license purchased only from themeforest(the above
     </div>
     <!--end::Scrolltop-->
     <!--begin::Sticky Toolbar-->
-<x-frontend.sticky></x-frontend.sticky>
+    <x-frontend.sticky></x-frontend.sticky>
 
     <script>
         var HOST_URL = "https://preview.keenthemes.com/metronic/theme/html/tools/preview";
